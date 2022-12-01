eb1637cb6584d1fe9d4cad4482930d53c92b3ef6
GGGeralt <Bartek.lipinski.pl@gmail.com>
2022-12-01 18:58:46 +0100

    Project1(1/12/2022)
    First project of Advent of Code 2022

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static int Main(string[] args)
+    {
+        List<int> maxes = new List<int>();
+        int sum = 0;
+
+        foreach (string line in File.ReadLines("file.txt"))
+        {
+
+            if (line == "")
+            {
+                maxes.Add(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += Convert.ToInt32(line);
+            }
+        }
+
+        sum = 0;
+        maxes.Sort();
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"{i} Miejsce: {maxes[maxes.Count - i]}");
+            sum += maxes[maxes.Count - i];
+        }
+
+        Console.WriteLine($"SUMA: {sum}");
+
+        return 0;
+    }
+}
